914d192d49686f2c9b50c32ca39a63ce88e48043
onomeh <onwuanibeonome@gmail.com>
2022-10-29 21:18:30 +0000

    Setup tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :posts, dependent: :destroy
+  
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
   def recent_posts
     posts.order(created_at: :DESC).limit(3)
