c6734cbf127cc2ffe68346fcb34a1812c4892dd8
onomeh <onwuanibeonome@gmail.com>
2022-10-29 09:11:15 +0000

    fix error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :posts, dependent: :destroy
 
-
   def recent_posts
     posts.order(created_at: :DESC).limit(3)
   end
